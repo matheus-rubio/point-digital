1a0f28b383ecb3a5faeb7309fae7609c3fd2edcb
Matheus Rubio <matheus.melo@smartnx.io>
2021-07-04 12:41:04 -0300

    Inicio do Projeto

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index(): Response
+    {
+        $data = [
+            'titulo' => 'Home',
+            'controller_name' => 'HomeController',
+        ];
+
+        return $this->render('home/index.html.twig', $data);
+    }
+}

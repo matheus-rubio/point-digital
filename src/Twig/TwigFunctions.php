d0b3cf136ac0f3632e822f33d1645b4bbfbd9f7b
Matheus Rubio <matheus.melo@smartnx.io>
2021-08-28 15:43:24 -0300

    Criaçao de funções twig

---
+++ src/Twig/TwigFunctions.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\TwigFunction;
+use Twig\Extension\AbstractExtension;
+
+class TwigFunctions extends AbstractExtension {
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('isAdministrador', [$this, 'isAdministrador']),
+            new TwigFunction('message', [$this, 'message']),
+        ];
+    }
+
+    public function isAdministrador(){
+
+        if (isset($_SESSION['isAdministrador'])){
+            return $_SESSION['isAdministrador'] ? true : false;
+        }
+    }
+
+    public function message(){
+
+        if (isset($_SESSION['message'])){
+            echo "<script> setTimeout(function () {
+				toastr.options = {
+					closeButton: true,
+					progressBar: true,
+					showMethod: 'slideDown',
+					timeOut: 4000
+				};
+				toastr.error(\"{$_SESSION['message']}\");
+			});</script>";
+            unset($_SESSION['message']);
+        }
+    }
+
+
+
+}

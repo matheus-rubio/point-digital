e9d537961f1ee94b6a5a59c6744d07aa1c1a051d
Matheus Rubio <matheus.melo@smartnx.io>
2021-08-29 20:14:50 -0300

    Gerenciamento de OS Funcional

---
+++ src/Controller/OrdensDeServicoController.php
@@ -0,0 +1,245 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Model\OrdensDeServicoModel;
+use App\Model\PedidosModel;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrdensDeServicoController extends AbstractController
+{
+    public $usuario;
+    public $ordensDeServicoModel;
+    
+    public function __construct()
+    {
+        if (isset($_SESSION) == false){
+            session_start();
+        }
+        $this->ordensDeServicoModel = new OrdensDeServicoModel;
+        $this->usuario = [
+            'nome' => isset($_SESSION['nome']) ? $_SESSION['nome'] : null,
+            'isAdministrador' => isset($_SESSION['isAdministrador']) ? $_SESSION['isAdministrador'] : null,
+        ];
+    }
+
+    /**
+     * @Route("/gerenciarOrdens", name="gerenciarOrdens")
+     */
+    public function gerenciarOrdens()
+    {
+        // SE ESTIVER LOGADO VERIFICA SE O USUÁRIO É ADMINISTRADOR
+        if (isset($_SESSION['isAdministrador'])) {
+
+            if ($_SESSION['isAdministrador'] == false) {
+
+                // SE NÃO FOR ADMINISTRADOR NÃO PERMITE ACESSAR A PAGINA
+                $_SESSION['message'] = [
+                    0 => 'error',
+                    1 => "Você não tem permissão para acessar esta página",
+                ];
+                return $this->redirect("/home");
+
+            } else {
+
+                $ordens = $this->ordensDeServicoModel->getTodasOrdens();
+                
+                for ($i=0; $i < sizeof($ordens); $i++) {
+                    ////////////// FORMATAÇÃO DO VALOR ORCAMENTO INICIAL ///////////////////
+                    // SE FOR NÚMERO INTEIRO ADICIONA DOIS ZEROS DEPOIS DA VIRGULA
+                    if(sizeof(explode('.', $ordens[$i]['orcamento_inicial'])) == 1){
+                        $ordens[$i]['orcamento_inicial'] = $ordens[$i]['orcamento_inicial'].",00";
+                    // SENÃO VERIFICA SE POSSUI UM NUMERO SÓ DEPOIS DA VIRGULA E ADICIONA UM ZERO  
+                    } else if(strlen(explode('.', $ordens[$i]['orcamento_inicial'])[1]) == 1){
+                        $ordens[$i]['orcamento_inicial'] = $ordens[$i]['orcamento_inicial']."0";
+                    }
+                     
+                    $ordens[$i]['orcamento_inicial'] = str_replace('.', ',', $ordens[$i]['orcamento_inicial']);
+
+                    ////////////// FORMATAÇÃO DO VALOR FINAL ///////////////////
+                    // SE FOR NÚMERO INTEIRO ADICIONA DOIS ZEROS DEPOIS DA VIRGULA
+                    if(sizeof(explode('.', $ordens[$i]['valor_final'])) == 1){
+                        $ordens[$i]['valor_final'] = $ordens[$i]['valor_final'].",00";
+                    // SENÃO VERIFICA SE POSSUI UM NUMERO SÓ DEPOIS DA VIRGULA E ADICIONA UM ZERO  
+                    } else if(strlen(explode('.', $ordens[$i]['valor_final'])[1]) == 1){
+                        $ordens[$i]['valor_final'] = $ordens[$i]['valor_final']."0";
+                    }
+                     
+                    $ordens[$i]['valor_final'] = str_replace('.', ',', $ordens[$i]['valor_final']);
+                }    
+
+                $data = [
+                    'titulo' => "Gerenciar OS's",
+                    'usuario' => $this->usuario,
+                    'ordens' => $ordens,
+                ];
+
+                return $this->render('/views/ordensDeServico/gerenciarOrdens.html.twig', $data);
+            }
+        } else {
+            // SE NÃO ESTIVAR LOGADO RETORNA PRA PAGINA INICIAL E APRESENTA UMA MENSAGEM DE ERRO
+            $_SESSION['message'] = [
+                0 => 'error',
+                1 => "Você precisa estar logado para acessar a página solicitada.",
+            ];
+            return $this->redirect("/home");
+        }
+        
+    }
+
+    /**
+     * @Route("/visualizarOS/{idOS}", name="visualizarOS", methods={"GET"})
+     */
+    public function visualizarOS(int $idOS)
+    {
+        $ordem = $this->ordensDeServicoModel->getOSByID($idOS);
+
+        // SE FOR NÚMERO INTEIRO ADICIONA DOIS ZEROS DEPOIS DA VIRGULA
+        if(sizeof(explode('.', $ordem['orcamento_inicial'])) == 1){
+            $ordem['orcamento_inicial'] = $ordem['orcamento_inicial'].",00";
+        // SENÃO VERIFICA SE POSSUI UM NUMERO SÓ DEPOIS DA VIRGULA E ADICIONA UM ZERO  
+        } else if(strlen(explode('.', $ordem['orcamento_inicial'])[1]) == 1){
+            $ordem['orcamento_inicial'] = $ordem['orcamento_inicial']."0";
+        }
+         
+        $ordem['orcamento_inicial'] = str_replace('.', ',', $ordem['orcamento_inicial']);
+
+        // SE FOR NÚMERO INTEIRO ADICIONA DOIS ZEROS DEPOIS DA VIRGULA
+        if(sizeof(explode('.', $ordem['valor_final'])) == 1){
+            $ordem['valor_final'] = $ordem['valor_final'].",00";
+        // SENÃO VERIFICA SE POSSUI UM NUMERO SÓ DEPOIS DA VIRGULA E ADICIONA UM ZERO  
+        } else if(strlen(explode('.', $ordem['valor_final'])[1]) == 1){
+            $ordem['valor_final'] = $ordem['valor_final']."0";
+        }
+         
+        $ordem['valor_final'] = str_replace('.', ',', $ordem['valor_final']);
+
+        $data = [
+            'titulo'    => 'Ordem de Serviço',
+            'ordem'   => $ordem,
+            'usuario'   => $this->usuario,
+        ];
+
+        return $this->render('/views/ordensDeServico/visualizarOS.html.twig', $data);
+    }
+
+    /**
+     * @Route("/novaOS", name="novaOS")
+     */
+    public function novaOS()
+    {
+        $clientes = $this->ordensDeServicoModel->getAllClients();
+
+        $status = [
+            "Pendente",
+            "Finalizado",
+            "Cancelado",
+        ];
+
+        $data = [
+            'titulo'    => 'Nova OS',
+            'clientes'  => $clientes,
+            'status'    => $status,
+            'usuario'   => $this->usuario,
+        ];
+
+        return $this->render('/views/ordensDeServico/novaOS.html.twig', $data);
+    }
+
+    /**
+     * @Route("/cadastrarOS", name="cadastrarOS", methods="POST")
+     */
+    public function cadastrarOS()
+    {
+        $ordem = [
+            'nome' => filter_var($_POST['nomePedido'], FILTER_SANITIZE_STRING),
+            'marca' => filter_var($_POST['marcaPedido'], FILTER_SANITIZE_STRING),
+            'preco' => str_replace(',', '.', filter_var($_POST['valorPedido'], FILTER_SANITIZE_STRING)),
+            'qnt_estoque' => filter_var($_POST['quantidadePedido'], FILTER_SANITIZE_NUMBER_INT),
+        ];
+
+        $foiInserido = $this->ordensDeServicoModel->insertOS($ordem);
+
+        if ($foiInserido){
+
+            $_SESSION['message'] = [
+                0 => 'success',
+                1 => "Pedido cadastrado com sucesso!",
+            ];
+
+            return $this->redirect("/gerenciarPedidos");
+
+        } else {
+
+            $_SESSION['message'] = [
+                0 => 'error',
+                1 => "Erro ao cadastrar o pedido!",
+            ];
+
+            return $this->redirect("/gerenciarPedidos");
+        }
+    }
+
+    /**
+     * @Route("/editarOS/{idOS}", name="editarOS", methods="GET")
+     */
+    public function editarOS($idOS)
+    {
+        $ordem = $this->ordensDeServicoModel->getOSById($idOS);
+
+        $status = [
+            "Pendente",
+            "Finalizado",
+            "Cancelado",
+        ];
+
+        $data = [
+            'titulo'    => 'Editar OS',
+            'idOS' => $idOS,
+            'ordem'   => $ordem,
+            'status'    => $status,
+            'usuario'   => $this->usuario,
+        ];
+
+        return $this->render('/views/ordensDeServico/editarOS.html.twig', $data);
+    }
+
+    /**
+     * @Route("/atualizarOS/{idOS}", name="atualizarOS", methods="POST")
+     */
+    public function atualizarOS($idOS)
+    {
+        
+        $ordem = [
+            'data_finalizacao'      => $_POST['data_finalizacao']       != null ? filter_var($_POST['data_finalizacao'])                                : null,
+            'modelo_aparelho'       => $_POST['modelo_aparelho']        != null ? filter_var($_POST['modelo_aparelho'], FILTER_SANITIZE_STRING)         : null,
+            'status_servico'        => $_POST['status_servico']         != null ? filter_var($_POST['status_servico'], FILTER_SANITIZE_STRING)          : null,
+            'problema_identificado' => $_POST['problema_identificado']  != null ? filter_var($_POST['problema_identificado'], FILTER_SANITIZE_STRING)   : null,
+            'orcamento_inicial'     => $_POST['orcamento_inicial']      != null ? filter_var($_POST['orcamento_inicial'], FILTER_SANITIZE_STRING)       : null,
+            'valor_final'           => $_POST['valor_final']            != null ? filter_var($_POST['valor_final'], FILTER_SANITIZE_STRING)             : null,
+        ];
+
+        $OSFoiAtualizada = $this->ordensDeServicoModel->updateOS($ordem, $idOS);
+
+        if ($OSFoiAtualizada){
+
+            $_SESSION['message'] = [
+                0 => 'success',
+                1 => "OS atualizada com sucesso!",
+            ];
+
+            return $this->redirect("/gerenciarOrdens");
+
+        } else {
+
+            $_SESSION['message'] = [
+                0 => 'error',
+                1 => "Erro ao atualizar a OS!",
+            ];
+
+            return $this->redirect("/gerenciarOrdens");
+        }
+    }    
+}
---
+++ src/Model/OrdensDeServicoModel.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Model;
+
+use App\Classes\Model;
+use PDO;
+use Symfony\Component\HttpFoundation\Response;
+
+class OrdensDeServicoModel extends Model
+{    
+    public function getTodasOrdens()
+    {
+        try {
+            $sql = "SELECT 
+                    id,
+                    id_cliente,
+                    to_char(data_inicio, 'dd/MM/YYYY HH24:MM') as data_inicio,
+                    to_char(data_finalizacao, 'dd/MM/YYYY HH24:MM') as data_finalizacao,
+                    modelo_aparelho,
+                    status_servico,
+                    problema_identificado,
+                    orcamento_inicial,
+                    valor_final
+                    FROM public.tb_ordens_de_servico
+                    ORDER BY id";
+            $select = $this->pdo->prepare($sql);
+            $select->execute();
+            return $select->fetchAll(PDO::FETCH_ASSOC);
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+        }
+    }
+
+    public function getOSByID($idOS)
+    {
+        try {
+            $sql = "SELECT 
+                    id,
+                    id_cliente,
+                    to_char(data_inicio, 'dd/MM/YYYY HH24:MM') as data_inicio,
+                    to_char(data_finalizacao, 'dd/MM/YYYY HH24:MM') as data_finalizacao,
+                    modelo_aparelho,
+                    status_servico,
+                    problema_identificado,
+                    orcamento_inicial,
+                    valor_final
+                    FROM public.tb_ordens_de_servico
+                    WHERE id = ?";
+            $select = $this->pdo->prepare($sql);
+            $select->bindValue(1, $idOS);
+            $select->execute();
+            return $select->fetch(PDO::FETCH_ASSOC);
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+        }
+    }
+
+    public function insertOS($ordem)
+    {
+        try {
+            $sql = "INSERT INTO public.tb_ordens_de_servico
+                    (id_cliente, id_produto, data_pedido, valor_total, status)
+                    VALUES(:id_cliente, :id_produto, :data_pedido, :valor_total, :status);
+            ";
+            $insert = $this->pdo->prepare($sql);
+            foreach ($ordem as $key => $value) {
+                $insert->bindValue(":{$key}", $value);
+            }
+            
+            return $insert->execute();
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+        }
+    }
+
+    public function updateOS($ordem, $idOS)
+    {
+        try {
+            $sql = "UPDATE public.tb_ordens_de_servico
+                    SET data_finalizacao=:data_finalizacao, 
+                        modelo_aparelho=:modelo_aparelho, 
+                        status_servico=:status_servico, 
+                        problema_identificado=:problema_identificado, 
+                        orcamento_inicial=:orcamento_inicial, 
+                        valor_final=:valor_final
+                    WHERE id={$idOS};
+            
+                    ";       
+            $update = $this->pdo->prepare($sql);
+            foreach ($ordem as $key => $value) {
+                if ($value == null){
+                    $update->bindValue(":{$key}", $value, PDO::PARAM_NULL);
+                } else {
+                    $update->bindValue(":{$key}", $value);
+                }
+            }
+
+            return $update->execute();
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+        }
+    }
+
+    public function deleteProduto($idOS)
+    {
+        try {
+            $sql = "DELETE FROM public.tb_ordens_de_servico
+                    WHERE id= ?;
+                    ";       
+            $delete = $this->pdo->prepare($sql);
+            $delete->bindValue(1, $idOS);
+            $delete->execute();
+            return $delete->rowCount() >= 1 ? new Response('1') : new Response('0');
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function getAllClients()
+    {
+        try {
+            $sql = "SELECT * FROM public.tb_clientes";
+            $select = $this->pdo->prepare($sql);
+            $select->execute();
+            return $select->fetchAll(PDO::FETCH_ASSOC);
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+        }
+    }
+
+}
